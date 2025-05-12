e4375a8c64ebac8c93bd643ae2b710cf9f6a3eda
kitamura <rkdgh19@naver.com>
2025-05-12 23:13:45 +0900

    feat(python): update url

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.post("/")
-async def webhook(request: Request):
+@app.post("/walrus")
+async def walrus(request: Request):
     data = await request.json()
     print("📩 Webhook 수신됨:", data)
 
-    # 예: Long 진입이면 주문 실행
+    # walusdt long
     if data.get("id") == "Long":
         print("➡️ 롱 포지션 진입 요청!")
         # place_order(side="Buy")
-
+    # walusdt short
     elif data.get("id") == "Short":
         print("⬅️ 숏 포지션 진입 요청!")
         # place_order(side="Sell")
